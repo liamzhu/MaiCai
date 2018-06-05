6a20683abf8d68711b134cb0514e9b30c9bbad09
liamzhu <2000liamzhu@gmail.com>
2018-06-05 13:59:32 +0800

    login

--- Client/Assets/Main/Scripts/Login/LoginControl.cs
+++ Client/Assets/Main/Scripts/Login/LoginControl.cs
@@ -1,20 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
 
 public class LoginControl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject uppercase;
+    public GameObject erroTip;
+    public Input userNameInput;
+    public Input passWordInput;
+
+    [DllImport("user32.dll", EntryPoint = "GetKeyboardState")]
+    public static extern int GetKeyboardState(byte[] pbKeyState);
+
+    public static bool CapsLockStatus
+    {
+        get
+        {
+            byte[] bs = new byte[256];
+            GetKeyboardState(bs);
+            return (bs[0x14] == 1);
+        }
+    }
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            //if (userNameInput)
+            //{
+            //    userNameInput.isSelected = false;
+            //    passWordInput.isSelected = true;
+            //}
+            //else
+            //{
+            //    userNameInput.isSelected = true;
+            //    passWordInput.isSelected = false;
+            //}
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            ClickLogin();
+        }
+
+        if (CapsLockStatus)
+        {
+            uppercase.SetActive(true);
+        }
+        else
+        {
+            uppercase.SetActive(false);
+        }
+    }
 
     public void ClickLogin() {
+
+    }
+
+    public void ClickOnRegister() {
+
+    }
+
+    public void ClickOnGetPass() {
 
     }
 }
